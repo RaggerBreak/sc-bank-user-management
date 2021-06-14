4df1ff26fa19dfef0134b08051420d2184d1013a
ragger.break <ragger.break@gmail.com>
2021-06-14 22:14:38 +0200

    axon configuration

--- user.cmd.api/src/main/java/com/springbank/user/cmd/api/UserCommandApplication.java
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/UserCommandApplication.java
@@ -1,8 +1,11 @@
 package com.springbank.user.cmd.api;
 
+import com.springbank.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({AxonConfig.class})
 @SpringBootApplication
 public class UserCommandApplication {
 
--- user.query.api/src/main/java/com/springbank/user/query/api/UserQueryApplication.java
+++ user.query.api/src/main/java/com/springbank/user/query/api/UserQueryApplication.java
@@ -1,8 +1,12 @@
 package com.springbank.user.query.api;
 
+import com.springbank.user.core.configuration.AxonConfig;
+import com.springbank.user.core.configuration.MongoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({AxonConfig.class, MongoConfig.class})
 @SpringBootApplication
 public class UserQueryApplication {
 
