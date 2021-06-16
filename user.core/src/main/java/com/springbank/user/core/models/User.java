6caf919f7c73bcb5beb4cf975e3c2992a8439de3
ragger.break <ragger.break@gmail.com>
2021-06-16 22:37:53 +0200

    refactor register user

--- user.core/src/main/java/com/springbank/user/core/models/User.java
+++ user.core/src/main/java/com/springbank/user/core/models/User.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -15,7 +16,7 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document(collation = "users")
+@Document(collection = "users")
 public class User {
 
     @Id
@@ -27,6 +28,7 @@ public class User {
     @Email(message = "please provide a valid email address")
     private String emailAddress;
     @NotNull(message = "please provide account credentials")
+    @Valid
     private Account account;
 
 }
