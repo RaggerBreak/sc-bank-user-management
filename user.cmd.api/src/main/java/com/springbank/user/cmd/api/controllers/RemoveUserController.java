8db79c55fe225167ad0a77c52083aa4314e7d93c
ragger.break <ragger.break@gmail.com>
2021-06-19 13:30:04 +0200

    add REST Controller for removing users

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/controllers/RemoveUserController.java
@@ -0,0 +1,37 @@
+package com.springbank.user.cmd.api.controllers;
+
+import com.springbank.user.cmd.api.commands.RemoveUserCommand;
+import com.springbank.user.cmd.api.dto.BaseResponse;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/removeUser")
+public class RemoveUserController {
+
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public RemoveUserController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BaseResponse> removeUser(@PathVariable String id) {
+
+        try {
+            commandGateway.send(new RemoveUserCommand(id));
+
+            return new ResponseEntity<>(new BaseResponse("User successfully deleted!"), HttpStatus.OK);
+
+        } catch (Exception e) {
+            var safeErrorMessage = "Error while processing delete user request for id: " + id;
+            System.out.println(e.toString());
+
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
